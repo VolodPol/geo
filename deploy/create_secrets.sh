0e585d408c57dfb8fe993f803527c8853213aac6
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2024-12-11 16:48:16 +0200

    update: changed image tag to 'latest'

--- deploy/create_secrets.sh
+++ deploy/create_secrets.sh
@@ -2,4 +2,4 @@ kubectl create secret generic secret-dockerhub \
 --from-file=.dockerconfigjson=/home/<your-username>/.docker/config.json \
 --type=kubernetes.io/dockerconfigjson
 
-kubectl create secret generic security --from-literal=security-password=<your-password>
+kubectl create secret generic security --from-literal=security-password='<your-password>'
